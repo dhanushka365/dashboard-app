e79b0f41e08fba23a5a44a398aaeb9c2c096c66f
dhanushka365 <technohub10000@gmail.com>
2022-06-10 22:05:07 +0530

    initial commit

---
+++ src/app/service/dashboard.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from "@angular/core";
+import {HttpClient} from '@angular/common/http'
+import { environment } from "src/environments/environment";
+import { Observable } from "rxjs";
+
+@Injectable({providedIn: 'root'})
+export class DashboardService{
+    private SERVER_URL = environment.serverUrl;
+
+    constructor(private http: HttpClient){}
+
+    public getHttpTraces(): Observable<any>{
+       return this.http.get(`${this.SERVER_URL}/httptrace`);
+    }
+}
