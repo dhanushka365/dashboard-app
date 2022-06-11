82608a7240666ff81baf51efab77e57736cb1f2c
dhanushka365 <technohub10000@gmail.com>
2022-06-11 21:20:37 +0800

    Http Traces

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,63 @@
-import { Component } from '@angular/core';
+import { OnInit,Component } from '@angular/core';
+import { SystemCpu } from './interface/system-cpu';
+import { SystemHealth } from './interface/system-health';
+import { DashboardService } from './service/dashboard.service';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'dashboard-app';
+export class AppComponent implements OnInit{
+  public traceList:any[]=[];
+  public selectedTrace: any;
+  //public systemHealth: SystemHealth;
+  //public systemCpu:SystemCpu;
+  //public processUptime:string;
+  public http200Traces: any[]=[];
+  public http400Traces: any[]=[];
+  public http404Traces: any[]=[];
+  public http500Traces: any[]=[];
+  public httpDefaultTraces: any[]=[];
+
+  constructor(private dashboardService:DashboardService){}
+  ngOnInit(): void {
+    this.getTraces();
+  }
+
+  private getTraces(): void{
+    this.dashboardService.getHttpTraces().subscribe(
+      (response:any)=>{
+        console.log(response.traces);
+        this.processTraces(response);
+
+      }
+    );
+
+  }
+
+  private processTraces(traces:any):void{
+     this.traceList =traces;
+     this.traceList.forEach(trace =>{
+      switch(trace.response.status){
+        case 200:
+          this.http200Traces.push(trace);
+          break;
+        case 400:
+          this.http400Traces.push(trace);
+          break;
+        case 404:
+          this.http404Traces.push(trace);
+          break;
+        case 500:
+            this.http500Traces.push(trace);
+            break;
+        default:
+            this.httpDefaultTraces.push(trace);
+            break;        
+      }
+     });
+    //throw new Error('Method not implemented.');
+  }
+
 }
